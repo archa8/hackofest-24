4f38894cf80609e60e156b143862d0e4a5efb20e
archa8 <archa.ug22.me@nitp.ac.in>
2024-04-07 17:53:39 +0530

    Add Firestore database integration and fetch user list in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,35 @@ import PropertyPage from './Pages/PropertyPage'
 import Chatbot from './components/Chatbot';
 import { Auth } from './components/auth';
 import './App.css';
+import { db } from './config/firebase';
+import { getDocs,collection } from 'firebase/firestore';
 
 function App() {
+  const [userList, setUserList] = useState([]);
+
+  const usersCollectionRef = collection(db, "users")
+
+  useEffect(() => {   
+    const getUserList = async () => {
+      // read data and set list
+      try{
+        const data= await getDocs(usersCollectionRef);
+        console.log(data);
+      } catch(err){
+        console.error(err);
+      }
+      
+    };
+    getUserList();
+
+  } , []);
+
+  const getUserList = async () => {
+    const usersRef = db.collection('users');
+    const snapshot = await usersRef.get();
+    const usersList = snapshot.docs.map(doc => doc.data());
+    setUserList(usersList);
+  }
   return (
     <>
     <Router>
